f45483c18646bbc24187838146a68776fe08ab35
RobertoCastelli <roberto.castelli.teal@gmail.com>
2022-06-09 00:04:51 +0200

    second commit

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,29 +1,44 @@
 const Footer = () => {
   return (
     <footer className="footer__container">
-      <div className="footer__terminal">TERMINAL</div>
+      <section className="footer__terminal">
+        <ul>
+          <li>PROBLEMS</li>
+          <li>OUTPUT</li>
+          <li>
+            <span className="footer__terminal--underline">TERMINAL</span>
+          </li>
+          <li>DEBUG CONSOLE</li>
+        </ul>
+      </section>
       <br />
-      <div className="footer__http">
-        <div>
-          local: <span>http://localhost:</span>3000
+
+      <section className="footer__http">
+        <div className="footer__http-local">Local:</div>
+        <div className="footer__http-local--host">
+          http://localhost:<b>3000</b>
         </div>
-        <div>
-          On Your Network: <span>http://127.0.0.1:</span>3000
+        <div className="footer__http-network">On Your Network:</div>
+        <div className="footer__http-network--host">
+          http://127.0.0.1:<b>3000</b>
         </div>
-      </div>
+      </section>
       <br />
-      <div className="footer__copyright">
-        copyright &copy; 2022 <span>Roberto Castelli</span>
-      </div>
+
+      <section className="footer__copyright">
+        Copyright &copy; 2022 <span>Roberto Castelli</span>.
+      </section>
       <br />
-      <div className="footer__webpack">
+
+      <section className="footer__webpack">
         <div>
           webpack compiled <span>successfully</span>
         </div>
         <div>No issues found.</div>
-      </div>
+      </section>
       <br />
-      <div className="footer__icons">icons</div>
+
+      <section className="footer__icons">icons</section>
     </footer>
   )
 }
