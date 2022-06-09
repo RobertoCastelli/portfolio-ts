758a271426167bc6b281cd5f91cadc17186013a0
RobertoCastelli <roberto.castelli.teal@gmail.com>
2022-06-09 16:29:46 +0200

    css flow

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -1,5 +1,23 @@
+import { VscFiles, VscGithubAlt, VscCode, VscBook } from "react-icons/vsc";
 const Menu = () => {
-  return <div className="menu__container">Menu</div>
-}
+  return (
+    <div className="menu__container">
+      <ul>
+        <li>
+          <VscFiles size={30} />
+        </li>
+        <li>
+          <VscGithubAlt size={30} />
+        </li>
+        <li>
+          <VscCode size={30} />
+        </li>
+        <li>
+          <VscBook size={30} />
+        </li>
+      </ul>
+    </div>
+  );
+};
 
-export default Menu
+export default Menu;
