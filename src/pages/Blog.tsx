5a30548725e59d23760ee118656d41ba33c26465
RobertoCastelli <roberto.castelli.teal@gmail.com>
2022-06-20 16:50:29 +0200

    added blog page

--- src/pages/Blog.tsx
+++ src/pages/Blog.tsx
@@ -14,27 +14,38 @@ const Blog = () => {
           <img src={sheep} alt="sheep" />
         </Link>
         <section>
-          "Impostor syndrome is a psychological pattern in which an individual
-          doubts their accomplishments despite external evidence of their
-          competence".
+          <div>{"<div className='wiki'>"}</div>
+          <div className="blog__text">
+            "Impostor syndrome is a psychological pattern in which an individual
+            doubts their accomplishments despite external evidence of their
+            competence".
+          </div>
         </section>
         <section>
-          Since I have a strong passion for programming languages, I recently
-          asked myself: why not bring into fruition my avid interest in this
-          field to enhance my career?
+          <div className="blog__text">
+            Since I have a strong passion for programming languages, I recently
+            asked myself: why not bring into fruition my avid interest in this
+            field to enhance my career?
+          </div>
         </section>
         <section>
-          This blog is a story in continuous evolution to keep track of my
-          progress, paths, errors, achievements in the hopes of helping other
-          people to share this adventure. Special thanks to Daniele Di Lucido &
-          Jaga Santagostino, my mentors.
-          <br />
-          <br />
-          Roberto
+          <div className="blog__text">
+            This blog is a story in continuous evolution to keep track of my
+            progress, paths, errors, achievements in the hopes of helping other
+            people to share this adventure. Special thanks to Daniele Di Lucido
+            & Jaga Santagostino, my mentors.
+          </div>
+          <section>
+            <div className="blog__text">Roberto</div>
+            <div>{"</div>"}</div>
+          </section>
         </section>
+
+        <div>{"<button onClick={goToPosts}>"}</div>
         <Link to="/posts">
           <button>posts</button>
         </Link>
+        <div>{"</button>"}</div>
       </div>
     </div>
   )
